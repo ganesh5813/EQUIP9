dafe65836810db0aad604704554b81bc0f1c0b2a
android9 <satish.g@mplussoft.com>
2020-03-06 14:59:40 +0530

    Changed in App.js

--- App.js
+++ App.js
@@ -4,7 +4,7 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Hello EQUIP9</Text>
+      <Text>Hello EQUIP9 Welcome</Text>
     </View>
   );
 }
